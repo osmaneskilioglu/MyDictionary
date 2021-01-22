ec4ef2303cc0bb00a0aaded552c96f26ae4a6090
OSMAN ESKILIOGLU <osmaneskilioglu@gmail.com>
2021-01-23 02:36:49 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        TKey[] tempkey;
+        TValue[] tempvalue;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            tempkey = keys;
+            tempvalue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                keys[i] = tempkey[i];
+                values[i] = tempvalue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+
+        }
+
+        public TKey[] GetKeys
+        {
+            get { return keys; }
+        }
+        public TValue[] GetValues
+        {
+            get { return values; }
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> age_name = new MyDictionary<int, string>();
+            age_name.Add(34, "Yamaç Kocovali");
+            age_name.Add(70, "İdris Kocovali");
+            age_name.Add(42, "Cumali Kocovali");
+
+            for (int i = 0; i < age_name.Length; i++)
+            {
+                Console.WriteLine("Age :" + age_name.GetKeys[i] + " - " + "Name Surname : " + age_name.GetValues[i]);
+            }
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Number of Records: " + " " + age_name.Length);
+            Console.ReadLine();
+        }
+    }
+}
